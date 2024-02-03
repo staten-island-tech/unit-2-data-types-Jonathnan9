c281a91484e05ab5d8de04de2afac2906bc1d4c7
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2024-02-02 22:04:19 -0500

    test

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-import turtle
-from turtle import *
-
-t = Turtle()
-t.speed(0)
-
-print("wow no errors")
-
-length=5
-
-def Square(length):
-    for i in range(4):
-        t.forward(length)
-        t.right(90)
-
-def SQuareSpiral(iRange):
-    length = 5
-    for i in range(iRange):
-        Square(length)
-        length += 5
-        t.right(5)
-SQuareSpiral(60)
-
-turtle.done()
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
