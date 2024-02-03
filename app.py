14fe50958a5fee84d5f899d23783a0038da6898b
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2024-02-02 21:59:46 -0500

    n

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ import turtle
 from turtle import *
 
 t = Turtle()
+t.speed(0)
 
-t.forward(200)
+print("wow no errors")
+
+length=5
+
+def Square(length):
+    for i in range(4):
+        t.forward(length)
+        t.right(90)
+
+def SQuareSpiral(iRange):
+    length = 5
+    for i in range(iRange):
+        Square(length)
+        length += 5
+        t.right(5)
+SQuareSpiral(60)
 
 turtle.done()
