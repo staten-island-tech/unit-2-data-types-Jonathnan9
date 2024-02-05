775a98a7de7c28343244c72192707b543427bc5b
Jonathnan9 <158211587+Jonathnan9@users.noreply.github.com>
2024-02-05 14:04:26 -0500

    trash

--- app.py
+++
@@ -1,7 +0,0 @@
-temp = 75
-if temp > 68:
-    print('warm')
-elif temp == 68:
-    print('perfect')
-else:
-    print('cold')
---
+++ challenge1.py
@@ -0,0 +1,10 @@
+""" word_counter = input("write something ")
+
+counter = word_counter.split( )
+
+ """
+
+x = input("write something")
+y= x.split( )
+
+print(y) 
