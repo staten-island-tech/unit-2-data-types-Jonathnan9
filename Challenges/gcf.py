f53088b7f8ac7a1e384b99217f795713b8f62fcc
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2024-02-14 14:46:59 -0500

    Update gcf.py

--- Challenges/gcf.py
+++ Challenges/gcf.py
@@ -1,26 +1,23 @@
 def gcf_calculator(numb1, numb2):
-  list1 = []
-  list2 =[]
+    list1 = []
+    list2 = []
 
-  for a in range(1, numb1+1):
-    if  numb1 % a == 0:
-      list1.append(a)
+    for a in range(1, numb1 + 1):
+        if numb1 % a == 0:
+            list1.append(a)
 
-  for b in range(1, numb2+1):
-    if  numb2 % b == 0:
-      list2.append(b)
+    for b in range(1, numb2 + 1):
+        if numb2 % b == 0:
+            list2.append(b)
 
-  print(list1,list2)
+    common_factors = set(list1) & set(list2)
+    gcf = max(common_factors)
 
+    return gcf
 
 
 numb1 = int(input("What is the first number? "))
-numb2 = int(input("What is the secomd number? "))
-gcf = gcf_calculator(numb1, numb2)
+numb2 = int(input("What is the second number? "))
 
-""" 
-if isinstance(gcf, str):
-    print(gcf)
-else:
-    print("The GCF is ", round(gcf, 2))
- """
+gcf = gcf_calculator(numb1, numb2)
+print("The greatest common factor is:", gcf)
