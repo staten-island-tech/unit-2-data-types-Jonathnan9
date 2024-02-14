940221dab4d24c44028fd19f1ca9eaa9cfe6cfaa
Jonathnan9 <158211587+Jonathnan9@users.noreply.github.com>
2024-02-14 13:57:16 -0500

    a

--- Challenges/gcf.py
+++ Challenges/gcf.py
@@ -1,8 +1,27 @@
 def gcf_calculator(numb1, numb2):
+  list1 = []
+  list2 =[]
+
+  for a in range(1, numb1+1):
+    if  numb1 % a == 0:
+      list1.append(a)
+
+  for b in range(1, numb2+1):
+    if  numb2 % b == 0:
+      list2.append(b)
+
+  for a in list1:
+    print(a, end=' ')
+
+numb1 = int(input("What is the first number? "))
+numb2 = int(input("What is the secomd number? "))
+gcf = gcf_calculator(numb1, numb2)
+""" 
+
     if numb1 > numb2:
-      gcf1 = numb1 - numb2
+      gcf = numb1 - numb2
     if numb1 < numb2:
-      gcf2 = numb2 - numb1
+      gcf = numb2 - numb1
     else:
      print('0')
 
@@ -17,3 +36,4 @@ if isinstance(gcf, str):
     print(gcf)
 else:
     print("The GCF is ", round(gcf, 2))
+ """
